31e3f50275ea2300104c2aea9e07dfe786220e77
fort726 <38447663+fort726@users.noreply.github.com>
2026-02-28 02:26:02 +0900

    cherry-pick ax

--- vllm/config/model.py
+++ vllm/config/model.py
@@ -1101,6 +1101,7 @@ class ModelConfig:
         elif self.hf_text_config.model_type in (
             "deepseek_v2",
             "deepseek_v3",
+            "AXK1",
             "deepseek_v32",
             "deepseek_mtp",
             "kimi_k2",
@@ -1115,7 +1116,7 @@ class ModelConfig:
             # underlying architecture
             return (
                 self.hf_text_config.model.model_type
-                in ("deepseek_v2", "deepseek_v3", "deepseek_v32")
+                in ("deepseek_v2", "deepseek_v3", "deepseek_v32", "AXK1")
                 and self.hf_text_config.kv_lora_rank is not None
             )
         return False
